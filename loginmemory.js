38235f61ae25b81d26de709d8059c270f6131a3c
larissambn <110267129+larissambn@users.noreply.github.com>
2022-08-25 14:44:15 -0300

    Update loginmemory.js

--- loginmemory.js
+++ loginmemory.js
@@ -1,6 +1,11 @@
 const input = document.querySelector(".login__input");
 const button = document.querySelector(".login__button");
 const form = document.querySelector(".login-form");
+const quizbutton = document.querySelector("#buttonquizz");
+
+quizbutton.addEventListener("click", () => {
+  window.location = "html/quizgame.html";
+});
 
 const validateInput = ({ target }) => {
   if (target.value.length > 2) {
@@ -15,7 +20,7 @@ const handleSubmit = (event) => {
   event.preventDefault();
 
   localStorage.setItem("player", input.value);
-  window.location = "pages/memorygame.html";
+  window.location = "html/memorygame.html";
 };
 
 input.addEventListener("input", validateInput);
